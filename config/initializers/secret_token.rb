58e530694bdc76137b8ad0fc847eface2e934ea8
marina-milicevic <marinam.milicevic@gmail.com>
2014-12-03 23:23:14 +0100

    init structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrustveniPortal::Application.config.secret_key_base = '6db1318a627460b7bd6c39e3753c82e1658de19a99173b0a2830c9dfec2609061e2613606773a16f85c15601729951b77e5fbafc4439c012fed4c6cb52bf731a'
