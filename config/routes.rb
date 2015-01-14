a9df06050c861832f2d1fc0990853fde153489d3
dervisevic-haris <90dervisevic.haris@gmail.com>
2015-01-14 02:46:58 +0100

    logout route moved to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DrustveniPortal::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get 'logout' => 'sessions#destroy', as: :log_out
   root 'home#index'
   resources :sessions, only: [:create]
   resources :users, only: [:new, :create, :show, :destroy]
@@ -10,5 +11,5 @@ DrustveniPortal::Application.routes.draw do
   get '/:category_name' => 'home#index', as: :filter
   get 'vote_up/:storage_id' => 'home#vote_up', as: :vote_up
   get 'vote_down/:storage_id' => 'home#vote_down', as: :vote_down
-  get 'logout' => 'sessions#destroy', as: :log_out
+  
 end
